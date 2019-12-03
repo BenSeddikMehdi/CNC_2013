72f41b1ef2dd20cd137bcfcdcf5eb4ee59acfe48
BenSeddikMehdi <amigoinpt@gmail.com>
2019-12-04 00:05:14 +0100

    Ending with the reversed Linked List path of A to B

--- main.c
+++ main.c
@@ -5,9 +5,7 @@
 int main() {
     pathList_t* temp_A_to_B = NULL, *temp_B_to_A = NULL;
     temp_A_to_B = AtoB();
-    printf("The address of the first Element (A-->B): %p %x\n", temp_A_to_B,  &temp_A_to_B);
     temp_B_to_A = BtoA();
-    printf("The address of the first Element (B-->A): %p %x", temp_B_to_A,  &temp_A_to_B);
     free(temp_A_to_B);
     free(temp_B_to_A);
     return 0;
